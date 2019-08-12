591b920b148f52e64d8bb420c9dc545ed23dc7ba
Whittle <matt.whittle10@gmail.com>
2019-08-11 23:01:42 -0600

    body parser package added to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
@@ -11,3 +12,16 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function(){
+    console.log("app is listening on " + PORT);
+})
