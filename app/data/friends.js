564676497d7d11e1db81e74e56da144b43abeee3
Whittle <matt.whittle10@gmail.com>
2019-08-11 20:51:19 -0600

    friend.js dummy data added in

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+    {
+        name: "Kevin",
+        url: "https://images.app.goo.gl/k4p4vAiU5B34UZ5M8",
+        scores: [2,5,2,3,5,1,3,4,2,5]
+    },
+    {
+        name: "Dwight",
+        url: "https://images.app.goo.gl/iwETiJo3WJFrdDVd9",
+        scores: [4,4,4,4,3,3,3,3,2,2]
+    },
+    {
+        name: "Michael",
+        url: "https://images.app.goo.gl/886NV1kNtUjWKZLR8",
+        scores: [3,3,3,3,3,3,3,1,1,5]
+    },
+    {
+        name: "Jim",
+        url: "https://images.app.goo.gl/eVQcC7WducQRE9DdA",
+        scores: [5,5,5,5,5,4,4,4,3,3]
+    },
+    {
+        name: "Pam",
+        url: "https://images.app.goo.gl/9CKEruMeP3bJuXaD6",
+        scores: [1,1,1,2,2,5,4,5,3,3]
+    },
+    {
+        name: "Angela",
+        url: "https://images.app.goo.gl/jSxujcEDyXFA34Ms6",
+        scores: [3,4,4,4,4,5,2,2,1,1]
+    },
+    {
+        name: "Phyllis",
+        url: "https://images.app.goo.gl/ZX6bSyF3ybFeHjc2A",
+        scores: [1,3,5,5,2,3,4,1,2,2]
+    },
+];
+
+module.exports = friends;
