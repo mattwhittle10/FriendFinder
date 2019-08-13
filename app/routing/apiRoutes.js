452a7dc07887a7d40cca776a89325edde4abb372
Whittle <matt.whittle10@gmail.com>
2019-08-12 22:52:31 -0600

    modal code added to html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,6 +14,24 @@ module.exports = function(app) {
         for (var i = 0; i < newUser.length; i++) {
             newUser.scores[i] = parseInt(newUser.scores[i]);
         }
+
+        // var bestFriendIndex = 0;
+        // var minDistance = 40;
+
+        // for(var i = 0; i < friends.length; i++) {
+        //     var totalDifference = 0;
+        //     for(var j = 0; j < friends[i].scores.length; j++) {
+        //       var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        //       totalDifference += difference;
+        //     }
+      
+        //     // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+        //     if(totalDifference < minimumDifference) {
+        //       bestFriendIndex = i;
+        //       minimumDifference = totalDifference;
+        //     }
+        //   }
+
         friends.push(newUser);
     });
 }
